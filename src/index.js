5587db13dd6d4f3d047e18b823f8258f23a15915
Dennis Glasberg <dennis.glasberg@flywire.com>
2020-07-14 11:27:01 -0400

    Added port for rest

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import rest from './rest';
-import graphql from './graphql';
+/* import graphql from './graphql'; */
 
-rest.listen({ port: 5000 }, () => {
+const PORT = process.env.PORT || 5000;
+
+rest.listen({ port: PORT }, () => {
   console.log(`REST API: Listening: http://localhost:5000`);
 });
 
+/*
 graphql.listen({ port: 4000 }, () => {
   console.log(`GRAPHQL API: Listening: http://localhost:4000`);
 });
+*/
