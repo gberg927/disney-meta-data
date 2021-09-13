1e33db1b4aef9dfde5d332e0323079601fc33bcd
Dennis Glasberg <dennis.glasberg@flywire.com>
2021-09-13 10:43:23 -0400

    Removed /

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ const corsOptions = {
   origin: [
     'http://localhost:3000',
     'https://disney-metadata-frontend.vercel.app',
-    'https://themeparkstats.com/',
+    'https://themeparkstats.com',
   ],
   credentials: true,
 };
